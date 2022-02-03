5fa1997d487f63537d3d9bb8159bc4c8d39b5430
Ivan-Porras <ivanporras44@gmail.com>
2022-02-03 14:45:50 +0100

    Light time bug solved

--- src/light/luz.cpp
+++ src/light/luz.cpp
@@ -1,40 +1,33 @@
-#include <iostream>
 #include "luz.h"
-
-using namespace std;
 
 
 Luz::Luz(int plot_number, bool init_status){
 
     plot_ident = plot_number;
     status = init_status;
+    last_switch_time = clock();
 
-    if(init_status){
-        last_on_time = clock();
-    }
 
 }
 
 void Luz::set_status(bool newstatus){
 
-    // Switch off/on
-    if((newstatus) && (status != newstatus)){
-        last_on_time = clock(); // Time refresh
+    // Switch off/on(new real status)
+    if(status != newstatus){
+        last_switch_time = clock(); // Time refresh
     }
 
     status = newstatus;
 }
 
-long long Luz::get_time(){
+double Luz::get_time(){
 
     clock_t final_time;
 
-    if(last_on_time == 0){final_time = 0;}
-    else{final_time = clock() - last_on_time;}
+    if(last_switch_time == 0){final_time = 0;}
+    else{final_time = clock() - last_switch_time;}
 
-    cout << final_time;
-
-    long long time = (long long)final_time / CLOCKS_PER_SEC;
+    double time = (double)final_time / CLOCKS_PER_SEC;
 
     return time;
 }
--- src/light/luz.h
+++ src/light/luz.h
@@ -14,7 +14,7 @@ class Luz{
         // Getters
         int get_plot_ident(){ return plot_ident;}
         bool get_status(){return status;}
-        long long get_time();
+        double get_time();
 
         //Setters
         void set_status(bool newstatus);
@@ -24,7 +24,7 @@ class Luz{
 
         int plot_ident = 0; // Number of plot
         bool status;        // On/of status
-        clock_t last_on_time;  // Save the last switch on time to calculates the total work time
+        clock_t last_switch_time;  // Save the last switch time
 
 };
 
