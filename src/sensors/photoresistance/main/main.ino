cdcd7b995106f8a07780f9cdfe43993bc4e5c705
Adrian Madinabeitia Portanova <a.madinabeitia.2020@alumnos.urjc.es>
2022-02-03 11:30:13 +0100

    Update espec.md

---
+++ src/sensors/photoresistance/main/main.ino
@@ -0,0 +1,27 @@
+#include "Sensor.h"
+#include "Photoresistance.h"
+
+  // Const 
+  const int PLOT_NUM = 2; 
+  const int GPIO_IN = 0; 
+
+  Photoresistance s2_photores(PLOT_NUM, GPIO_IN);
+
+void setup() 
+{
+   Serial.begin(9600);
+}
+
+void loop() 
+{ 
+
+      s2_photores.update_value();
+      Serial.println( "VALUE : ");
+      Serial.println(s2_photores.get_value());
+   //   Serial.println( "CONT : ");
+   //   Serial.println(s1_humedity.get_cont());
+   //   Serial.println( "PLOT : ");
+   //   Serial.println(s1_humedity.get_plot_ident());
+      delay(1000);
+
+}
